14f87539a97ed51d523ded1a6140c4448ba01f7d
Jason Röhm <137874576+dotJasonRoehm@users.noreply.github.com>
2025-12-04 21:00:19 +0100

    init logly local

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,16 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 import * as schema from "./schema";
 
-export const db = drizzle({
-  client: sql,
+if (!process.env.POSTGRES_URL) {
+  throw new Error("Missing POSTGRES_URL environment variable");
+}
+
+// Create the connection
+const connection = postgres(process.env.POSTGRES_URL);
+
+export const db = drizzle(connection, {
   schema,
   casing: "snake_case",
 });
